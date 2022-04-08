fe8a52365c771a2a52c0cf1c81fb8ee45a08ce5c
Payhaev17 <ran777dom@gmail.com>
2022-04-08 14:04:57 +0300

    changes

--- js/functions/handleProjectBtns.js
+++ js/functions/handleProjectBtns.js
@@ -2,6 +2,7 @@ export default function handleProjectsBtns(side, projects) {
   if (side === "right") {
     projects.style.transform = "translateX(-514px)";
   } else if (side === "left") {
+    projects.style.transform = "translateX(0px)";
   } else {
     return;
   }
