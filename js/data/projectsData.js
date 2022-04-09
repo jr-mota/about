a461dd9d2c1d7b0fa9d3e848865cc1e89c86a5c6
Payhaev17 <ran777dom@gmail.com>
2022-04-09 15:05:07 +0300

    changes

--- js/data/projectsData.js
+++ js/data/projectsData.js
@@ -4,17 +4,17 @@ export default {
   list: [
     {
       name: "React Pizza",
-      src: "../assets/posts/React Pizza.png",
+      src: "./assets/posts/React Pizza.png",
       projectHref: "https://github.com/jr-mota/react-pizza",
     },
     {
       name: "Ghost Pay",
-      src: "../assets/posts/Ghost Pay.png",
+      src: "./assets/posts/Ghost Pay.png",
       projectHref: "https://ghostpay.ru/",
     },
     {
       name: "Atom",
-      src: "../assets/posts/Atom.png",
+      src: "./assets/posts/Atom.png",
       projectHref: "https://github.com/jr-mota/vue-electron-app",
     },
   ],
